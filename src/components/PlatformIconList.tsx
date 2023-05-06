fb4c604a81cc91367381d678cd38bdba933ce023
Haydn <lizexu@Haydn.local>
2023-05-06 16:49:41 -0700

    Genre Selection Completed

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -27,7 +27,7 @@ const PlatformIconList = ({ platforms }: Props) => {
     return (
         <HStack marginY={'10px'}>
             {platforms.map((platform) =>
-                <Icon as={iconMap[platform.slug]} color='pink.200' />
+                <Icon key={platform.id} as={iconMap[platform.slug]} color='pink.200' />
             )}
         </HStack>
     )
